bcaa3a738371aa5f2658fbba1ee9caaf299d3c44
AlanZ <alanzheng2004@outlook.com>
2025-11-11 00:32:12 -0500

    change header buttons, and added animation

--- src/ui/src/components/header.tsx
+++ src/ui/src/components/header.tsx
@@ -1,16 +1,35 @@
 import {HeaderState} from '../state/header_state';
+import "./header.css"
 
 export function Header(state: HeaderState) {
     return (
         <header>
             <div className="w-full flex flex-wrap">
-                [<a href="#/home">home</a>
-                &nbsp; &nbsp;
-                <a href="#/eventlog">eventlog</a>
-                &nbsp; &nbsp;
-                <a href="#/foodlog">foodlog</a>
-                &nbsp; &nbsp;
-                <a href="#/foods">foods</a>]
+                <div className="button-bar">
+                    <a href="#/home" className="header-button">
+                        <span className="button-content">
+                            Home
+                        </span>
+                    </a>
+                    &nbsp; &nbsp;
+                    <a href="#/eventlog" className="header-button">
+                        <span className="button-content">
+                            Event Log
+                        </span>    
+                    </a>
+                    &nbsp; &nbsp;
+                    <a href="#/foodlog" className="header-button">
+                        <span className="button-content">
+                            Food Log
+                        </span>    
+                    </a>
+                    &nbsp; &nbsp;
+                    <a href="#/foods" className="header-button">
+                        <span className="button-content">
+                            Foods
+                        </span>    
+                    </a>
+                </div>
                 <span className="ml-auto">
                     [<a href="#/login">login</a>
                     &nbsp; &nbsp;
