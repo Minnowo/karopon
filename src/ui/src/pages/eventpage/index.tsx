3106c0a50f8934b6d004e5f3be09eb4f27072875
Minnowo <62451415+Minnowo@users.noreply.github.com>
2025-12-11 16:44:40 -0500

    improve event page panel display

--- src/ui/src/pages/eventpage/index.tsx
+++ src/ui/src/pages/eventpage/index.tsx
@@ -1,13 +1,16 @@
-import {useState} from 'preact/hooks';
+import {useRef, useState} from 'preact/hooks';
 import {BaseState} from '../../state/basestate';
-import {CreateUserEventLog, TblUserFoodLog, UserEventLogWithFoodLog} from '../../api/types';
+import {CreateUserEventLog, TblUser, TblUserFoodLog, UserEventLogWithFoodLog} from '../../api/types';
 import {ApiNewEventLog} from '../../api/api';
 import {formatSmartTimestamp} from '../../utils/date_utils';
-import {DropdownButton} from '../../components/drop_down_button';
+import {DropdownButton, DropdownButtonAction} from '../../components/drop_down_button';
 import {DownloadData, GenerateEventTableText} from '../../utils/download';
 import {AddEventsPanel} from './add_event_panel';
 import {UserEventLogWithFoodLogFactory} from '../../api/factories';
 import {NumberInput} from '../../components/number_input';
+import {CalculateCalories, Str2CalorieFormula} from '../../utils/calories';
+import {Fragment} from 'preact/jsx-runtime';
+import {DoRender} from '../../hooks/doRender';
 
 export function EventsPage(state: BaseState) {
     const [showNewEventPanel, setShowNewEventPanel] = useState<boolean>(false);
@@ -90,92 +93,148 @@ export function EventsPage(state: BaseState) {
 
             <div className={`w-full space-y-4 ${showNewEventPanel && 'mt-4'}`}>
                 {state.eventlogs.length === 0 && (
-                    <div className="text-center font-bold p-32">
-                        Shhh… the list is taking a nap right now. Try adding something to wake it up!
+                    <div className="text-center font-bold">
+                        Shhh… the list is taking a nap right now.
+                        <br /> Try adding something to wake it up!
                     </div>
                 )}
-                {state.eventlogs.slice(0, numberToShow).map((foodGroup: UserEventLogWithFoodLog) => {
-                    return (
-                        <div key={foodGroup.eventlog.id} className="w-full p-2 border container-theme">
-                            <div className="flex flex-row flex-wrap w-full justify-between align-middle">
-                                <span className="text-s font-semibold">{`${foodGroup.eventlog.event} `}</span>
-                                <span className=" text-s font-semibold">
-                                    {formatSmartTimestamp(foodGroup.eventlog.user_time)}
-                                </span>
-                                <DropdownButton
-                                    actions={[
-                                        {
-                                            label: 'Copy',
-                                            onClick: () => {
-                                                setNewEvent({...foodGroup});
-                                                setShowNewEventPanel(true);
-                                                window.scrollTo({top: 0, behavior: 'smooth'});
-                                            },
-                                        },
-                                        {label: 'Edit', onClick: () => {}},
-                                        {label: 'Delete', onClick: () => {}},
-                                    ]}
-                                />
-                            </div>
-                            <div className="flex flex-row flex-wrap w-full justify-evenly">
-                                <span className="whitespace-nowrap">{`Blood Sugar: ${foodGroup.eventlog.blood_glucose}`}</span>
-                                <span className="whitespace-nowrap">
-                                    {`Insulin Taken: ${foodGroup.eventlog.actual_insulin_taken}`}
-                                </span>
-                            </div>
+                {state.eventlogs.slice(0, numberToShow).map((foodGroup: UserEventLogWithFoodLog) => (
+                    <EventPanel
+                        key={foodGroup.eventlog.id}
+                        user={state.user}
+                        foodGroup={foodGroup}
+                        actions={[
+                            {
+                                label: 'Copy',
+                                onClick: () => {
+                                    setNewEvent({...foodGroup});
+                                    setShowNewEventPanel(true);
+                                    window.scrollTo({top: 0, behavior: 'smooth'});
+                                },
+                            },
+                            {label: 'Edit', onClick: () => {}},
+                            {label: 'Delete', onClick: () => {}},
+                        ]}
+                    />
+                ))}
+            </div>
+        </>
+    );
+}
 
-                            {foodGroup.foodlogs.length > 0 && (
-                                <div className="w-full mt-2 overflow-x-scroll">
-                                    <table className="w-full text-sm border-collapse">
-                                        <thead>
-                                            <tr className="font-semibold text-xs border-b">
-                                                <th className="text-left py-1"> </th>
-                                                <th className="text-right py-1 pr-1">Amount</th>
-                                                <th className="text-right py-1 pr-1">Protein</th>
-                                                <th className="text-right py-1 pr-1">Carbs</th>
-                                                <th className="text-right py-1 pr-1">Fibre</th>
-                                                <th className="text-right py-1 pr-1">Fat</th>
-                                                <th className="text-right py-1">NetCarbs</th>
-                                            </tr>
-                                        </thead>
+interface EventPanelState {
+    user: TblUser;
+    foodGroup: UserEventLogWithFoodLog;
+    actions: DropdownButtonAction[];
+}
+export function EventPanel({user, foodGroup, actions}: EventPanelState) {
+    const [curRow, setCurRow] = useState<number>(-1);
 
-                                        <tbody>
-                                            {foodGroup.foodlogs.length > 1 && (
-                                                <tr>
-                                                    <td className="whitespace-nowrap">Total</td>
-                                                    <td className="text-right">-</td>
-                                                    <td className="text-right pr-1">{foodGroup.total_protein.toFixed(1)}</td>
-                                                    <td className="text-right pr-1">{foodGroup.total_carb.toFixed(1)}</td>
-                                                    <td className="text-right pr-1">{foodGroup.total_fibre.toFixed(1)}</td>
-                                                    <td className="text-right pr-1">{foodGroup.total_fat.toFixed(1)}</td>
-                                                    <th className="text-right">
-                                                        {' '}
-                                                        {(foodGroup.total_carb - foodGroup.total_fibre).toFixed(1)}{' '}
-                                                    </th>
+    return (
+        <>
+            <div className="w-full p-2 border container-theme">
+                <div className="flex flex-row flex-wrap w-full justify-between align-middle">
+                    <span className="text-s font-semibold">{`${foodGroup.eventlog.event} `}</span>
+                    <span className=" text-s font-semibold">{formatSmartTimestamp(foodGroup.eventlog.user_time)}</span>
+                    <DropdownButton actions={actions} />
+                </div>
+
+                <div className="flex flex-row flex-wrap w-full justify-evenly">
+                    <span className="">{`BloodSugar ${foodGroup.eventlog.blood_glucose.toFixed(1)}`}</span>
+                    <span className="mx-1">{`Insulin ${foodGroup.eventlog.actual_insulin_taken.toFixed(1)}`}</span>
+                    <span>
+                        {`Callories ${CalculateCalories(
+                            foodGroup.total_protein,
+                            foodGroup.total_carb,
+                            foodGroup.total_fibre,
+                            foodGroup.total_fat,
+                            Str2CalorieFormula(user.caloric_calc_method)
+                        ).toFixed(1)}`}
+                    </span>
+                </div>
+
+                {foodGroup.foodlogs.length > 0 && (
+                    <div className="w-full mt-2 overflow-x-scroll">
+                        <table className="w-full text-sm border-collapse">
+                            <thead>
+                                <tr className="text-xs border-b">
+                                    <th className="font-normal text-left py-1"> </th>
+                                    <th className="font-normal text-right py-1 pr-2" title="Amount">
+                                        Amt
+                                    </th>
+                                    <th className="font-normal text-right py-1 pr-2" title="Protein">
+                                        Prot
+                                    </th>
+                                    <th className="font-normal text-right py-1 pr-2" title="Carbs">
+                                        Carb
+                                    </th>
+                                    <th className="font-normal text-right py-1 pr-2" title="Fibre">
+                                        Fib
+                                    </th>
+                                    <th className="font-normal text-right py-1 pr-2" title="Fat">
+                                        Fat
+                                    </th>
+                                    <th className="font-normal text-right py-1" title="Net Carbs">
+                                        Net
+                                    </th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                {foodGroup.foodlogs.length > 1 && (
+                                    <tr>
+                                        <td className="whitespace-nowrap">Total</td>
+                                        <td className="text-center">-</td>
+                                        <td className="text-right pr-2">{foodGroup.total_protein.toFixed(1)}</td>
+                                        <td className="text-right pr-2">{foodGroup.total_carb.toFixed(1)}</td>
+                                        <td className="text-right pr-2">{foodGroup.total_fibre.toFixed(1)}</td>
+                                        <td className="text-right pr-2">{foodGroup.total_fat.toFixed(1)}</td>
+                                        <th className="text-right">
+                                            {(foodGroup.total_carb - foodGroup.total_fibre).toFixed(1)}{' '}
+                                        </th>
+                                    </tr>
+                                )}
+                                {foodGroup.foodlogs.map((food: TblUserFoodLog, i: number) => {
+                                    const shown = i == curRow;
+                                    const toggle = () =>  setCurRow(shown ? -1 : i) ;
+                                    return (
+                                        <Fragment key={food.id}>
+                                            {shown && (
+                                                <tr className="cursor-pointer" onClick={toggle}>
+                                                    <td className="border-c-l-green border-t border-l " colSpan={7}>
+                                                    <div className="mx-1">
+                                                    {food.name}
+                                                    </div>
+                                                    </td>
                                                 </tr>
                                             )}
-                                            {foodGroup.foodlogs.map((food: TblUserFoodLog) => (
-                                                <tr key={food.id}>
-                                                    <td className="whitespace-nowrap max-w-[150px] pr-1">
-                                                        <div className="overflow-x-scroll">{food.name}</div>
-                                                    </td>
-                                                    <td className="text-right whitespace-nowrap pr-1">
-                                                        {food.portion} {food.unit}
-                                                    </td>
-                                                    <td className="text-right pr-1">{food.protein.toFixed(1)}</td>
-                                                    <td className="text-right pr-1">{food.carb.toFixed(1)}</td>
-                                                    <td className="text-right pr-1">{food.fibre.toFixed(1)}</td>
-                                                    <td className="text-right pr-1">{food.fat.toFixed(1)}</td>
-                                                    <td className="text-right">{(food.carb - food.fibre).toFixed(1)}</td>
-                                                </tr>
-                                            ))}
-                                        </tbody>
-                                    </table>
-                                </div>
-                            )}
-                        </div>
-                    );
-                })}
+                                            <tr onClick={toggle} className="cursor-pointer">
+                                                <td className="whitespace-nowrap max-w-[100px] sm:w-full pr-2">
+                                                    {!shown ? (
+                                                        <div className="overflow-x-hidden">{food.name}</div>
+                                                    ) : (
+                                                        <div className="border-c-l-green border-b border-l w-full">
+                                                        &nbsp;
+                                                        </div>
+                                                    )}
+                                                </td>
+                                                <td className="text-right whitespace-nowrap pr-2">
+                                                    {' '}
+                                                    {food.portion} {food.unit}{' '}
+                                                </td>
+                                                <td className="text-right pr-2">{food.protein.toFixed(1)}</td>
+                                                <td className="text-right pr-2">{food.carb.toFixed(1)}</td>
+                                                <td className="text-right pr-2">{food.fibre.toFixed(1)}</td>
+                                                <td className="text-right pr-2">{food.fat.toFixed(1)}</td>
+                                                <td className="text-right">{(food.carb - food.fibre).toFixed(1)}</td>
+                                            </tr>
+                                        </Fragment>
+                                    );
+                                })}
+                            </tbody>
+                        </table>
+                    </div>
+                )}
             </div>
         </>
     );
